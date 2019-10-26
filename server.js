a9dcf462e9d18d051dd838900122ec05852fcb71
Jesaiah <jesaiahprayor@gmail.com>
2019-10-26 01:40:25 -0400

    c

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use('/',routes);
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header("Access-Control-Allow-Headers", 'Authorization, Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     // intercept OPTIONS method
     if ('OPTIONS' == req.method) {
