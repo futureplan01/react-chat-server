d33c5e336fdf3858871f27360d202b82e2be53e5
Jesaiah <jesaiahprayor@gmail.com>
2019-10-26 01:52:42 -0400

    sockets comment out

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const server = app.listen(port,()=>{
     console.log("Server running on http://localhost:" + port);
 })
 
-
+/*
 const io = require("socket.io").listen(server);
 
 io.on("connection", (client)=>{
@@ -29,5 +29,6 @@ io.on("connection", (client)=>{
         console.log(msg);
     });
 })
+*/
 
 
