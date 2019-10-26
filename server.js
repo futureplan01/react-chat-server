5ff0493e4177ba9bfbbc3534562fa7f85872a204
Jesaiah <jesaiahprayor@gmail.com>
2019-10-25 22:43:36 -0400

    added routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const db = require('./model/connectDB');
 const bodyParser = require('body-parser');
+const routes = require('./routes/api');
 const app = express();
 let port = process.env.PORT || 7555;
 
@@ -9,9 +10,11 @@ db.connect();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cors());
+app.use('/',routes);
 const server = app.listen(port,()=>{
     console.log("Server running on http://localhost:" + port);
 })
+
 
 const io = require("socket.io").listen(server);
 
