12128e760cbfd7e2e1190fd4dd4c57cfbb5ac8f5
Jesaiah <jesaiahprayor@gmail.com>
2019-10-26 01:37:26 -0400

    changed header

--- server.js
+++ server.js
@@ -11,6 +11,19 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use('/',routes);
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Authorization, Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    } else {
+        next();
+    }
+});
 const server = app.listen(port,()=>{
     console.log("Server running on http://localhost:" + port);
 })
