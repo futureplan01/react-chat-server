61f9ed60879ecc04c2d9799b89999d53c98f7150
Jesaiah <jesaiahprayor@gmail.com>
2019-11-01 13:33:20 -0400

    no change

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ const server = app.listen(port,()=>{
     console.log("Server running on http://localhost:" + port);
 })
 
-
 const io = require("socket.io").listen(server);
 
 io.on("connection", (client)=>{
