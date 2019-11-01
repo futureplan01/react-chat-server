34a78b3c44da21d04ae2eeac09cab7994fd6d5f3
Jesaiah <jesaiahprayor@gmail.com>
2019-11-01 13:38:03 -0400

    deleted chat

--- server.js
+++ server.js
@@ -17,18 +17,6 @@ const server = app.listen(port,()=>{
     console.log("Server running on http://localhost:" + port);
 })
 
-const io = require("socket.io").listen(server);
-
-io.on("connection", (client)=>{
-    console.log("a user is connected");
-    client.on("disconnect", ()=>{
-        console.log("user has disconnected");
-    });
-    client.on("server", msg =>{
-        client.broadcast.emit("user", msg);
-        console.log(msg);
-    });
-})
 
 
 
